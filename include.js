bc41cb59b34b1dfc4454ccf11dc5d1d688f5f634
Sammy-T <cyam9019@aim.com>
2021-04-08 05:54:41 -0400

    Switch to data attributes

--- include.js
+++ include.js
@@ -1,15 +1,15 @@
 /**
- * Searches for all elements with the 'w3-include-html' attribute,
+ * Searches for all elements with the 'data-include-html' attribute,
  * uses the attribute value to fetch the specified file, and inserts
  * the file's contents into the element.
  */
 function includeHTML() {
     /**
      * A helper to fetch and insert included file contents into the container element.
-     * @param {Element} includeEl - An element with the 'w3-include-html' attribute.
+     * @param {Element} includeEl - An element with the 'data-include-html' attribute.
      */
     async function insertContent(includeEl) {
-        const includeFile = includeEl.getAttribute('w3-include-html');
+        const includeFile = includeEl.getAttribute('data-include-html');
 
         const response = await fetch(includeFile);
         if(!response.ok) return;
@@ -21,7 +21,7 @@ function includeHTML() {
     }
 
     // Find the includes and insert their content
-    const includes = document.querySelectorAll('[w3-include-html]');
+    const includes = document.querySelectorAll('[data-include-html]');
     includes.forEach(include => insertContent(include));
 }
 
